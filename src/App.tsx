d5a2bda3593575711c7de1b6c92c3740b6a6bd62
Athenkosi Mamfengu <53196964+Athistic@users.noreply.github.com>
2025-03-22 16:48:26 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-// import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-// import Navbar from './components/molecules/nav-bar';
-// import ProductsPage from './components/pages/products';
-// import SingleProduct from './components/organisms/single-product';
-// import Paths from './shared/paths';
+import Navbar from './components/molecules/nav-bar';
+import ProductsPage from './components/pages/products';
+import SingleProduct from './components/organisms/single-product';
+import Paths from './shared/paths';
 
 function App() {
   return (
-    <>Testing my APP</>
-    // <Router basename="/shopping-app-prac">
-    //   <Navbar />
-    //   <Routes>
-    //     <Route path={Paths.home} element={<>Home page coming soon!!</>} />
-    //     <Route path={Paths.products} element={<ProductsPage />} />
-    //     <Route path={Paths.singleProduct} element={<SingleProduct />} />
-    //   </Routes>
-    // </Router>
+    <Router basename="/shopping-app-prac">
+      <Navbar />
+      <Routes>
+        <Route path={Paths.home} element={<>Home page coming soon!!</>} />
+        <Route path={Paths.products} element={<ProductsPage />} />
+        <Route path={Paths.singleProduct} element={<SingleProduct />} />
+      </Routes>
+    </Router>
   );
 }
 
